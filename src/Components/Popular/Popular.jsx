4c25dd77545183eafdf7cd9fabdaad23c60aef98
/ <gautamsingh728296@gmail.com>
2026-04-22 11:24:47 +0530

    initial commit

---
+++ src/Components/Popular/Popular.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './Popular.css'
+import Item from "../Item/Item"
+import data_product from '../../assets/data'
+
+const Popular = () => {
+  return (
+    <div className='popular'>
+      <h1>POPULAR IN WOMEN</h1>
+      <hr/>
+      <div className='popular-item'>
+        {data_product.map((item,i)=>{
+          return <Item key={i} id={item.id} name={item.name} image={item.image}/>
+        })}
+      </div>
+    </div>
+  )
+}
+
+export default Popular
